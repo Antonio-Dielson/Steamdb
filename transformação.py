445cafce155d0bb92377b46c5df17fe9ae81a3f3
Antonio Dielson Vieira da Silva <hammstark0@gmail.com>
2024-11-07 18:19:11 -0300

    Add files via upload

--- transformação.py
+++ transformação.py
@@ -11,13 +11,17 @@ print(dfsteam.info())
 '''
 
 Transformações: remoção de colunas nulas e textos desnecessários na coluna Name,
-renomeação de coluna e salvar o DataFrame em formato csv com as alterações.
+renomeação de coluna, reomoção de caracteres especiais e tipagem de coluna Price,
+formatação de coluna Release e salvar o DataFrame em formato csv com as alterações.
 
 '''
 dfsteam.drop(columns=['Unnamed: 0', 'Unnamed: 1', 'Ends', 'Started'], inplace=True)
 dfsteam['Name']= dfsteam['Name'].str.split('\n').str[0]
 dfsteam.rename(columns={'%':'Discount_rate'}, inplace=True)
+dfsteam['Price']= dfsteam['Price'].str.replace('R$ ', '').str.replace(',', '.').astype(float)
+dfsteam['Release']= pd.to_datetime(dfsteam['Release'], format='%b %Y')
+
 
 dfsteam.to_csv('silver_steam.csv', index=False)
-df = pd.read_csv('silver_steam.csv')
-print(df.head(10))
+#df = pd.read_csv('silver_steam.csv')
+#print(df.head(10))
